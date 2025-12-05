aa048e3cfe3eeed7d18fcf397d72047ecc971e44
Antihype-K <antihypekpss123@gmail.com>
2025-12-06 00:48:21 +0400

    added HTTP methods

--- main.go
+++ main.go
@@ -28,168 +28,187 @@ func sliceStringToInt(sliceString []string) []int {
 }
 
 func deleteHandler(w http.ResponseWriter, r *http.Request) {
-	httpRequestBody, err := io.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		fmt.Println("Ошибка при прочтении запроса ", err)
-		return
-	}
-	if len(httpRequestBody) == 0 {
-		w.Write([]byte("Отправьте ключи для удаления (числа через пробел)\n"))
-		return
-	}
-	stringes := string(httpRequestBody)
-	splits := strings.Fields(stringes)
-	splitInt := sliceStringToInt(splits)
-
-	mtx.Lock()
-	var mapDelete map[int]string = make(map[int]string)
-	var notFound = make([]int, 0)
-	for _, split := range splitInt {
-		if value, exists := maper[split]; exists {
-			delete(maper, split)
-			mapDelete[split] = value
-		} else {
-			notFound = append(notFound, split)
+	if r.Method == "DELETE" {
+		httpRequestBody, err := io.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			fmt.Println("Ошибка при прочтении запроса ", err)
+			return
 		}
-	}
+		if len(httpRequestBody) == 0 {
+			w.Write([]byte("Отправьте ключи для удаления (числа через пробел)\n"))
+			return
+		}
+		stringes := string(httpRequestBody)
+		splits := strings.Fields(stringes)
+		splitInt := sliceStringToInt(splits)
 
-	if len(mapDelete) > 0 {
-		fmt.Println("\n" + strings.Repeat("=", 50))
-		fmt.Println("Удаленные элементы")
-		for key, value := range mapDelete {
-			fmt.Println("Удален ключ ", key, "со значением ", value)
+		mtx.Lock()
+		var mapDelete map[int]string = make(map[int]string)
+		var notFound = make([]int, 0)
+		for _, split := range splitInt {
+			if value, exists := maper[split]; exists {
+				delete(maper, split)
+				mapDelete[split] = value
+			} else {
+				notFound = append(notFound, split)
+			}
+		}
+
+		if len(mapDelete) > 0 {
 			fmt.Println("\n" + strings.Repeat("=", 50))
+			fmt.Println("Удаленные элементы")
+			for key, value := range mapDelete {
+				fmt.Println("Удален ключ ", key, "со значением ", value)
+				fmt.Println("\n" + strings.Repeat("=", 50))
+			}
+			if len(notFound) > 0 {
+				fmt.Println("Ключ ", notFound, "не найден")
+			}
+			fmt.Println("В мапе осталось ", len(maper), " ключей")
 		}
-		if len(notFound) > 0 {
-			fmt.Println("Ключ ", notFound, "не найден")
+		mtx.Unlock()
+
+		w.Header().Set("Content-Type", "application/json")
+
+		response := map[string]interface{}{
+			"success":   true,
+			"deleted":   mapDelete,
+			"not_found": notFound,
+			"total":     len(maper),
+			"message":   fmt.Sprintf("Удалено: %d, Не найдено: %d", len(mapDelete), len(notFound)),
 		}
-		fmt.Println("В мапе осталось ", len(maper), " ключей")
-	}
-	mtx.Unlock()
 
-	w.Header().Set("Content-Type", "application/json")
-
-	response := map[string]interface{}{
-		"success":   true,
-		"deleted":   mapDelete,
-		"not_found": notFound,
-		"total":     len(maper),
-		"message":   fmt.Sprintf("Удалено: %d, Не найдено: %d", len(mapDelete), len(notFound)),
-	}
-
-	if err := json.NewEncoder(w).Encode(response); err != nil {
-		fmt.Println("Ошибка отправки JSON:", err)
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			fmt.Println("Ошибка отправки JSON:", err)
+		}
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Неправильный метод, воспользуйтесь DELETE"))
 	}
 
 }
 
 func addHandler(w http.ResponseWriter, r *http.Request) {
-	httpRequestBody, err := io.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		http.Error(w, "Ошибка при прочтении http тела", http.StatusBadRequest)
-		fmt.Println("Ошибка при прочтении http тела", err)
-		return
-	}
-
-	stringes := string(httpRequestBody)
-	splits := strings.Fields(stringes)
-	mtx.Lock()
-
-	var mapAdd map[int]string = make(map[int]string)
-	maxID := 0
-	for id := range maper {
-		if id > maxID {
-			maxID = id
+	if r.Method == "POST" {
+		httpRequestBody, err := io.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			http.Error(w, "Ошибка при прочтении http тела", http.StatusBadRequest)
+			fmt.Println("Ошибка при прочтении http тела", err)
+			return
 		}
-	}
 
-	for i, split := range splits {
-		key := maxID + i + 1
-		maper[key] = split
-		mapAdd[key] = split
-	}
-	mtx.Unlock()
-	if len(mapAdd) > 0 {
-		for key, value := range mapAdd {
-			fmt.Println("Добавлено значение ", value, "с ключом ", key)
+		stringes := string(httpRequestBody)
+		splits := strings.Fields(stringes)
+		mtx.Lock()
+
+		var mapAdd map[int]string = make(map[int]string)
+		maxID := 0
+		for id := range maper {
+			if id > maxID {
+				maxID = id
+			}
 		}
-	}
-	w.Header().Set("Content-Type", "application/json")
 
-	response := map[string]interface{}{
-		"success": true,
-		"added":   mapAdd,
-		"total":   len(maper),
-		"message": fmt.Sprintf("Добавлено %d элементов", len(mapAdd)),
-	}
+		for i, split := range splits {
+			key := maxID + i + 1
+			maper[key] = split
+			mapAdd[key] = split
+		}
+		mtx.Unlock()
+		if len(mapAdd) > 0 {
+			for key, value := range mapAdd {
+				fmt.Println("Добавлено значение ", value, "с ключом ", key)
+			}
+		}
+		w.Header().Set("Content-Type", "application/json")
 
-	json.NewEncoder(w).Encode(response)
+		response := map[string]interface{}{
+			"success": true,
+			"added":   mapAdd,
+			"total":   len(maper),
+			"message": fmt.Sprintf("Добавлено %d элементов", len(mapAdd)),
+		}
+
+		json.NewEncoder(w).Encode(response)
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Неправильный метод, воспользуйтесь POST"))
+	}
 }
 
 func printHandler(w http.ResponseWriter, r *http.Request) {
-	mtx.RLock()
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(maper)
-	fmt.Println(maper)
-	mtx.RUnlock()
+	if r.Method == "GET" {
+		mtx.RLock()
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(maper)
+		fmt.Println(maper)
+		mtx.RUnlock()
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Неправильный метод, воспользуйтесь GET"))
+	}
 }
 
 func getByID(w http.ResponseWriter, r *http.Request) {
-	httpRequestBody, err := io.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		http.Error(w, "Ошибка при прочтении http тела", http.StatusBadRequest)
-		fmt.Println("Ошибка при прочтении http тела", err)
-		return
-	}
-
-	if len(httpRequestBody) == 0 {
-		w.Write([]byte("Отправьте ключи для получения значений (числа через пробел)\n"))
-		return
-	}
-	stringes := string(httpRequestBody)
-	splits := strings.Fields(stringes)
-	splitInt := sliceStringToInt(splits)
-
-	infoID := make(map[int]string)
-	notFound := make([]int, 0)
-	mtx.RLock()
-	defer mtx.RUnlock()
-	for _, id := range splitInt {
-		if value, exists := maper[id]; exists {
-			infoID[id] = value
-			fmt.Printf("Найдено: ID=%d -> %s\n", id, value)
-		} else {
-			w.WriteHeader(http.StatusNotFound)
-			notFound = append(notFound, id)
-
+	if r.Method == "GET" {
+		httpRequestBody, err := io.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			http.Error(w, "Ошибка при прочтении http тела", http.StatusBadRequest)
+			fmt.Println("Ошибка при прочтении http тела", err)
+			return
 		}
 
-	}
-	w.Header().Set("Content-Type", "application/json")
+		if len(httpRequestBody) == 0 {
+			w.Write([]byte("Отправьте ключи для получения значений (числа через пробел)\n"))
+			return
+		}
+		stringes := string(httpRequestBody)
+		splits := strings.Fields(stringes)
+		splitInt := sliceStringToInt(splits)
 
-	if len(notFound) > 0 {
-		// Если есть ненайденные - отправляем 404
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode(map[string]interface{}{
-			"error":     "Некоторые сообщения не найдены",
-			"found":     infoID,
-			"not_found": notFound,
-			"total":     len(maper),
-		})
+		infoID := make(map[int]string)
+		notFound := make([]int, 0)
+		mtx.RLock()
+		defer mtx.RUnlock()
+		for _, id := range splitInt {
+			if value, exists := maper[id]; exists {
+				infoID[id] = value
+				fmt.Printf("Найдено: ID=%d -> %s\n", id, value)
+			} else {
+				w.WriteHeader(http.StatusNotFound)
+				notFound = append(notFound, id)
+
+			}
+
+		}
+		w.Header().Set("Content-Type", "application/json")
+
+		if len(notFound) > 0 {
+			// Если есть ненайденные - отправляем 404
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"error":     "Некоторые сообщения не найдены",
+				"found":     infoID,
+				"not_found": notFound,
+				"total":     len(maper),
+			})
+		} else {
+			// Если все найдены - отправляем 200
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"success": true,
+				"found":   infoID,
+				"total":   len(infoID),
+			})
+		}
 	} else {
-		// Если все найдены - отправляем 200
-		json.NewEncoder(w).Encode(map[string]interface{}{
-			"success": true,
-			"found":   infoID,
-			"total":   len(infoID),
-		})
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Неправильный метод, воспользуйтесь GET"))
 	}
-
 }
 
 func main() {
